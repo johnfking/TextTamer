0b4dc7b4a67d0956ae025b30b5ed4b7b113d8a4a
John <414568+johnfking@users.noreply.github.com>
2024-05-09 17:09:12 -0400

    feat: adds bylines

--- src/App.tsx
+++ src/App.tsx
@@ -50,58 +50,66 @@ function App() {
             placeholder="Type or paste your text here for analysis..."
             rows={parseInt("10")}
             cols={parseInt("50")}
-            />
+         />
          <br />
          <button onClick={calculateReadability}>Calculate Results</button>
          {readingEase !== null && gradeLevel !== null && (
-            <div className="results">
-               <h3>Analysis</h3>
-               <div className="result-grid">
-               <div>
-                  <strong>Text Standard:</strong> <br />
-                  <em>Provides an overall assessment of the text's readability using various measures.</em>
+            <div className="container">
+               <div className="results">
+                  <h3>Analysis</h3>
+                  <div className="result-grid">
+                     <div>
+                        <strong>Text Standard:</strong> <br />
+                        <em>Provides an overall assessment of the text's readability using various measures.</em>
+                     </div>
+                     <div>{textStandard}</div>
+                     <div>
+                        <strong>Flesch Reading Ease:</strong> <br />
+                        <em>Measures how easy the text is to read. Higher scores mean easier reading. Scores range from 0 to 100.</em>
+                     </div>
+                     <div>{readingEase}</div>
+                     <div>
+                        <strong>Flesch-Kincaid Grade Level:</strong> <br />
+                        <em>Indicates the U.S. school grade level needed to understand the text.</em>
+                     </div>
+                     <div>{gradeLevel}</div>
+                     <div>
+                        <strong>Coleman-Liau Index:</strong> <br />
+                        <em>Estimates the U.S. school grade level based on the number of letters and sentences.</em>
+                     </div>
+                     <div>{colemanLiau}</div>
+                     <div>
+                        <strong>Automated Readability Index (ARI):</strong> <br />
+                        <em>Determines the U.S. grade level required to comprehend the text, based on characters per word.</em>
+                     </div>
+                     <div>{ari}</div>
+                     <div>
+                        <strong>Dale-Chall Readability Score:</strong> <br />
+                        <em>Considers familiar words to determine the grade level needed for comprehension.</em>
+                     </div>
+                     <div>{daleChall}</div>
+                     <div>
+                        <strong>Difficult Words:</strong> <br />
+                        <em>Number of words considered difficult to understand.</em>
+                     </div>
+                     <div>{difficultWords}</div>
+                     <div>
+                        <strong>Linsear Write Formula:</strong> <br />
+                        <em>Computes the U.S. grade level needed for understanding based on sentence length and complex words.</em>
+                     </div>
+                     <div>{linsearWrite}</div>
+                     <div>
+                        <strong>Gunning Fog Index:</strong> <br />
+                        <em>Calculates the years of formal education needed to understand the text.</em>
+                     </div>
+                     <div>{gunningFog}</div>
+                  </div>
                </div>
-               <div>{textStandard}</div>
-                  <div>
-                     <strong>Flesch Reading Ease:</strong> <br />
-                     <em>Measures how easy the text is to read. Higher scores mean easier reading. Scores range from 0 to 100.</em>
-                  </div>
-                  <div>{readingEase}</div>
-                  <div>
-                     <strong>Flesch-Kincaid Grade Level:</strong> <br />
-                     <em>Indicates the U.S. school grade level needed to understand the text.</em>
-                  </div>
-                  <div>{gradeLevel}</div>
-                  <div>
-                     <strong>Coleman-Liau Index:</strong> <br />
-                     <em>Estimates the U.S. school grade level based on the number of letters and sentences.</em>
-                  </div>
-                  <div>{colemanLiau}</div>
-                  <div>
-                     <strong>Automated Readability Index (ARI):</strong> <br />
-                     <em>Determines the U.S. grade level required to comprehend the text, based on characters per word.</em>
-                  </div>
-                  <div>{ari}</div>
-                  <div>
-                     <strong>Dale-Chall Readability Score:</strong> <br />
-                     <em>Considers familiar words to determine the grade level needed for comprehension.</em>
-                  </div>
-                  <div>{daleChall}</div>
-                  <div>
-                     <strong>Difficult Words:</strong> <br />
-                     <em>Number of words considered difficult to understand.</em>
-                  </div>
-                  <div>{difficultWords}</div>
-                  <div>
-                     <strong>Linsear Write Formula:</strong> <br />
-                     <em>Computes the U.S. grade level needed for understanding based on sentence length and complex words.</em>
-                  </div>
-                  <div>{linsearWrite}</div>
-                  <div>
-                     <strong>Gunning Fog Index:</strong> <br />
-                     <em>Calculates the years of formal education needed to understand the text.</em>
-                  </div>
-                  <div>{gunningFog}</div>
+               <div className="byline">
+                  <p>
+                     Created using the <a href="https://www.npmjs.com/package/text-readability" target="_blank" rel="noopener noreferrer">text-readability</a> library.<br />
+                     Check out the project on <a href="https://github.com/johnfking/texttamer" target="_blank" rel="noopener noreferrer">GitHub</a>.
+                  </p>
                </div>
             </div>
          )}
