574cd5d22c77c603e72b143295b1e3525b7274fa
John <king.johnf@gmail.com>
2025-09-23 06:34:55 -0400

    Add GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   base: "/TextTamer",
-   plugins: [react()],
+  base: '/TextTamer/',
+  plugins: [react()],
 })
